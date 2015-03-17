db5d014deb43c88e5cf84f34b1389d9d4deeec81
JoeMcNelly <joemcnelly1@gmail.com>
2015-03-17 19:26:16 -0400

    Unit Test Lab

--- ExpediaTest/FlightTest.cs
+++ ExpediaTest/FlightTest.cs
@@ -7,6 +7,49 @@ namespace ExpediaTest
 	[TestFixture()]
 	public class FlightTest
 	{
-		//TODO Task 7 items go here
+        private readonly DateTime leaveDate = new DateTime(2013, 10, 6);
+        
+        [Test()]
+        public void TestThatFlightInitializes()
+        {
+            DateTime secondDate = new DateTime(2013, 10, 7);
+            var target = new Flight(leaveDate, secondDate, 500);
+            Assert.IsNotNull(target);
+        }
+        [Test()]
+        public void TestOneDayFlightCost()
+        {
+            DateTime secondDate = new DateTime(2013, 10, 7);
+            var target = new Flight(leaveDate, secondDate, 500);
+            Assert.AreEqual(220, target.getBasePrice());
+        }
+        [Test()]
+        public void TestTwoDayFlightCost()
+        {
+            DateTime secondDate = new DateTime(2013, 10, 8);
+            var target = new Flight(leaveDate, secondDate, 500);
+            Assert.AreEqual(240, target.getBasePrice());
+        }
+        [Test()]
+        public void TestTenDayFlightCost()
+        {
+            DateTime secondDate = new DateTime(2013, 10, 16);
+            var target = new Flight(leaveDate, secondDate, 500);
+            Assert.AreEqual(400, target.getBasePrice());
+        }
+        [Test()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestThatFlightHandlesNegativeMiles()
+        {
+            DateTime secondDate = new DateTime(2013, 10, 7);
+            new Flight(leaveDate, secondDate, -100);
+        }
+        [Test()]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void TestThatFlightHandlesBadDates()
+        {
+            DateTime secondDate = new DateTime(2013, 9, 7);
+            new Flight(leaveDate, secondDate, 100);
+        }
 	}
 }
